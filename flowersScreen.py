8a065476c138a98f463f9efd002f5587efbe0168
krismadden <madden_kris@yahoo.com>
2018-03-11 19:03:55 +0100

    Update flowersScreen.py

--- flowersScreen.py
+++ flowersScreen.py
@@ -7,34 +7,34 @@ import scrollphathd
 
 #Global Stuff
 
-numberstringKM89 = ""
-messagesKM89 = ["You had me at aloe.", 
-	    "I'd like to spend more thyme with you", 
-	    "Please don't ever leaf me.", 
-	    "Stop. Hammer thyme.", 
-	    "I'm rooting for you!", 
-	    "Leaf me alone.", 
-	    "It's spring. We are so excited we wet our plants!", 
-	    "I'm awesome, dill with it.", 
-	    "Say aloe to my little friend.", 
-	    "I hope thistle cheer you up.",
-	   "Chive had my eye on you.",
-	   "Don't squash my dreams.",
-	   "Peas, Love, and Happiness.",
-	   "I bean longing for your company.",
-	   "I'm meloncholy without you.",
-	   "We make a great pear.",
-	   "I always have a berry nice time with you.",
-	   "Turnip the party!",
-	   "Will you rosemary me?",
-	   "You're my missing peas.",
-	   "This is acorny pun."]
-messageKM89 = ""
-responseKM89 = ""
-number_krisKM89 = "0033637165118"
-numberKM89 = ""
-YorNKM89 = ""
-endKM89 = False
+# numberstringKM89 = ""
+# messagesKM89 = ["You had me at aloe.", 
+# 	    "I'd like to spend more thyme with you", 
+# 	    "Please don't ever leaf me.", 
+# 	    "Stop. Hammer thyme.", 
+# 	    "I'm rooting for you!", 
+# 	    "Leaf me alone.", 
+# 	    "It's spring. We are so excited we wet our plants!", 
+# 	    "I'm awesome, dill with it.", 
+# 	    "Say aloe to my little friend.", 
+# 	    "I hope thistle cheer you up.",
+# 	   "Chive had my eye on you.",
+# 	   "Don't squash my dreams.",
+# 	   "Peas, Love, and Happiness.",
+# 	   "I bean longing for your company.",
+# 	   "I'm meloncholy without you.",
+# 	   "We make a great pear.",
+# 	   "I always have a berry nice time with you.",
+# 	   "Turnip the party!",
+# 	   "Will you rosemary me?",
+# 	   "You're my missing peas.",
+# 	   "This is acorny pun."]
+# messageKM89 = ""
+# responseKM89 = ""
+# number_krisKM89 = "0033637165118"
+# numberKM89 = ""
+# YorNKM89 = ""
+# endKM89 = False
 
 
 #def textInfo( ):
