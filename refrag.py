6573cd36ce140ff0af58b93df0e6ec68baec2961
krismadden <madden_kris@yahoo.com>
2018-03-27 19:47:09 +0200

    Update refrag.py

--- refrag.py
+++ refrag.py
@@ -69,7 +69,8 @@ readit()
 
 serialport.write("AT+CMGS=\"" + number + "\"\n")
 serialport.write(message+"\r")
-serialport.write("\x1A") #ctrlz
+serialport.write("\0x1A") #ctrlz
+serialport.write("0x1A") #ctrlz
 readit()
 
 
