869085c27529a0dfd3262b240b2d5fcc9290ef62
krismadden <madden_kris@yahoo.com>
2018-05-08 16:35:20 +0200

    Update speaking.py

--- speaking.py
+++ speaking.py
@@ -8,14 +8,23 @@
 import sys
 import pyttsx
 
-def main():
-	print 'running speaking.py'
+def speak(str speakThis):
 	engine = pyttsx.init()
 	str = "how are you doing? c'est une belle maison."
 	if len(sys.argv) > 1:
 		str = sys.argv[1]
 	engine.say(str)
 	engine.runAndWait()
+
+def main():
+# 	print 'running speaking.py'
+# 	engine = pyttsx.init()
+# 	str = "how are you doing? c'est une belle maison."
+# 	if len(sys.argv) > 1:
+# 		str = sys.argv[1]
+# 	engine.say(str)
+# 	engine.runAndWait()
+	speak("how are you doing?")
 	while True:
 		input = raw_input("Enter Something to Say or type EXIT to exit.\n")
 		if input == "EXIT" or input == "exit" or input == "Exit":
